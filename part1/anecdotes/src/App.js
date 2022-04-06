fb2647b3e390620a0c4292ffb3e9d6763a2dfb40
TimHa91 <45972852+timHa91@users.noreply.github.com>
2022-04-06 11:45:19 +0200

    1.14: anecdotes step3

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+
+let trackGeneratedNumber = 0;
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+  const [mostVoted, setMostVoted] = useState('')
+
+  const getRandomInt = () => Math.floor(Math.random() * anecdotes.length)
+
+  const randomAnecdote = () => {
+    let generatedNumber = getRandomInt();
+    // Avoid repeating
+    while (generatedNumber === trackGeneratedNumber) {
+      generatedNumber = getRandomInt()
+    }
+    trackGeneratedNumber = generatedNumber;
+
+    setSelected(generatedNumber)
+
+  }
+  const setVote = () => {
+    const copyPoints = points.concat()
+    copyPoints[selected] += 1
+    setPoints(copyPoints)
+    findAndSetMostVoted(copyPoints)
+  }
+
+  const findAndSetMostVoted = (array) => {
+    let max = array[0]
+    array.forEach(e => {
+      if (max < e)
+        max = e
+    })
+    setMostVoted(array.indexOf(max))
+  }
+
+  return (
+    <div>
+      <OfTheDay anecdote={anecdotes[selected]} points={points[selected]}/>
+      <button onClick={setVote}>vote</button>
+      <button onClick={randomAnecdote}>next anecdote</button>
+      <MostVotes anecdote={anecdotes[mostVoted]} points={points[mostVoted]} />
+    </div>
+  )
+}
+
+const MostVotes = (props) => {
+  const header = "Anecdote with most votes"
+  if (props.points)
+    return (
+      <div>
+        <h1>{header}</h1>
+        <p>{props.anecdote}</p>
+        <p>has {props.points} votes</p>
+      </div>
+    )
+  else
+    return (
+      <div>
+        <h1>{header}</h1>
+        <p>none</p>
+      </div>
+    )
+}
+
+const OfTheDay = (props) => {
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdote}</p>
+      <p>has {props.points} votes</p>
+    </div>
+  )
+}
+
+export default App
